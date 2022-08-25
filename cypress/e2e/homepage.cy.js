675a4b4a665c13e3b50c23a3b06af90d65e82b78
Kevin Old <kevin@kevinold.com>
2022-08-25 13:54:02 -0500

    wip - iterate on cypress scaffold

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,8 +1,40 @@
-describe('home page functionality', () => {
-  it('should render posts using SSR', () => {
-    cy.visit('/')
-    cy.getBySel('posts-count').should("have.text", 2)
+/// <reference types="cypress" />
 
-    cy.getBySelLike('post-').should("have.length", 2)
-  })
-})
+describe("shared todos functionality", () => {
+  beforeEach(() => {
+    cy.fixture("users").then((usersJson) => {
+      cy.loginWithCognitoUI(usersJson[0].username, Cypress.env("testPassword"));
+      cy.visit("/");
+    });
+
+    cy.intercept("POST", Cypress.env("AppSyncGraphQLEndpoint"), (req) => {
+      if (req.body.hasOwnProperty("query") && req.body.query.includes("query ListTodos")) {
+        req.alias = "gqlListTodosQuery";
+      }
+
+      if (req.body.hasOwnProperty("query") && req.body.query.includes("mutation CreateTodo")) {
+        req.alias = "gqlCreateTodoMutation";
+      }
+    });
+    cy.visit("/");
+  });
+
+  it("displays the home page", () => {
+    cy.getBySel("nav-dashboard").should("be.visible");
+
+    cy.getBySel("header-title").should("have.text", "Dashboard");
+
+    cy.getBySel("meal-list-loading").should("exist");
+
+    cy.wait("@gqlMealsByOwnerQuery");
+
+    cy.getBySel("meal-list-loading").should("not.exist");
+  });
+
+  it("should render posts using SSR", () => {
+    cy.visit("/");
+    cy.getBySel("posts-count").should("have.text", 2);
+
+    cy.getBySelLike("post-").should("have.length", 2);
+  });
+});
