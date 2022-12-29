3d2afb99874830911ce945469b5d8d996229ae54
Kevin Old <kevin@kevinold.com>
2022-12-29 17:06:47 -0600

    add code to joinTodoUserGroupFunc

--- amplify/backend/function/joinTodoUserGroupFunc/src/index.js
+++ amplify/backend/function/joinTodoUserGroupFunc/src/index.js
@@ -6,18 +6,80 @@
 	REGION
 Amplify Params - DO NOT EDIT */
 
+import { API } from "@aws-amplify/api";
+
+const config = {
+  aws_project_region: process.env.REGION,
+  // @ts-ignore
+  aws_appsync_graphqlEndpoint: process.env.API_202208231425AMPLIFYC_GRAPHQLAPIENDPOINTOUTPUT,
+  aws_appsync_region: process.env.REGION,
+  aws_appsync_authenticationType: "API_KEY",
+  aws_appsync_apiKey: process.env.API_202208231425AMPLIFYC_GRAPHQLAPIKEYOUTPUT,
+};
+
+API.configure(config);
+
+const todoUserGroupByInviteCode = /* GraphQL */ `
+  query TodoUserGroupByInviteCode($inviteCode: String = "") {
+    todoUserGroupByInviteCode(inviteCode: $inviteCode) {
+      items {
+        id
+      }
+    }
+  }
+`;
+
+const updateTodoUserGroup = /* GraphQL */ `
+  mutation UpdateTodoUserGroup(
+    $input: UpdateTodoUserGroupInput!
+    $condition: ModelTodoUserGroupConditionInput
+  ) {
+    updateTodoUserGroup(input: $input, condition: $condition) {
+      id
+    }
+  }
+`;
+
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
-exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
-    };
+export const handler = async (event) => {
+  console.log(`EVENT: ${JSON.stringify(event)}`);
+  const { userId, inviteCode } = event.arguments.input;
+
+  let todoUserGroup;
+
+  try {
+    const res = await API.graphql({
+      query: todoUserGroupByInviteCode,
+      variables: { inviteCode },
+    });
+
+    if (res.data.todoUserGroupByInviteCode?.items && res.data.todoUserGroupByInviteCode.items[0]) {
+      todoUserGroup = res.data.todoUserGroupByInviteCode?.items[0];
+    }
+  } catch (error) {
+    console.log(error);
+    throw new Error(JSON.stringify(error, null, 2));
+  }
+
+  if (!todoUserGroup) return "Invalid Invite Code or group is full";
+
+  try {
+    const res = await API.graphql({
+      query: updateTodoUserGroup,
+      variables: {
+        input: {
+          id: todoUserGroup.id,
+          // append userId to existing owners
+          owners: [...todoUserGroup.owners, userId],
+        },
+      },
+    });
+
+    return { statusCode: 200, body: "Success" };
+  } catch (error) {
+    console.log(error);
+    throw new Error(JSON.stringify(error, null, 2));
+  }
 };
